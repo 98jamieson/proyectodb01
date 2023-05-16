29bc30bb10f0e3436a3582b67844e38b7100fee5
98jamieson <jamilemus@gmail.com>
2023-05-15 22:30:54 -0600

    Commitdeprueba

---
+++ Controllers/PruebaController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class PruebaController
+        Inherits Controller
+
+        ' GET: Prueba
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
